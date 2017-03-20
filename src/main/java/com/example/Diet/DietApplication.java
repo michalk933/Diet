967ddc6305df94b109dd16151f24d21b0f70c3f0
michalk933 <6pak.tv@gmail.com>
2017-03-20 22:34:56 +0100

    Initial commit

---
+++ src/main/java/com/example/Diet/DietApplication.java
@@ -0,0 +1,12 @@
+package com.example.Diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietApplication {
+//new
+	public static void main(String[] args) {
+		SpringApplication.run(DietApplication.class, args);
+	}
+}
